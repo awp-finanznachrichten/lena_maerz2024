2d6ff60a92198b1948e257cceb08013e3a55004e
awp-finanznachrichten <sw@awp.ch>
2024-02-12 09:47:59 +0100

    commit from Rstudio

--- lena_run.R
+++ lena_run.R
@@ -51,7 +51,7 @@ source("kantonale_abstimmungen_special.R", encoding="UTF-8")
 
 #Make Commit
 git2r::config(user.name = "awp-finanznachrichten",user.email = "sw@awp.ch")
-token <- read.csv("C:/Users/simon/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
+token <- read.csv("C:/Users/sw/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
 git2r::cred_token(token)
 gitadd()
 gitcommit()
@@ -64,7 +64,7 @@ if (time_check_national == FALSE) {
 #source("./top_flop/top_flop_chartbuilder_it-2144682.R", encoding = "UTF-8")
 
 #Make Commit
-token <- read.csv("C:/Users/simon/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
+token <- read.csv("C:/Users/sw/OneDrive/Github_Token/token.txt",header=FALSE)[1,1]
 git2r::cred_token(token)
 gitadd()
 gitcommit()
