8b370dd19b794d768d472b586aca97af7238e9f5
awp-finanznachrichten <sw@awp.ch>
2024-02-29 13:28:32 +0100

    commit from Rstudio

--- Auswertungen/age_korrelation.R
+++ Auswertungen/age_korrelation.R
@@ -1,6 +1,15 @@
 library(readxl)
+library(ggpubr)
+age_korrelation <- na.omit(read_excel("Data/age_korrelation.xlsx"))
 
-(cor(age_korrelation$`Anteil ü65-Jährige`,age_korrelation$`Ja-Anteil 13. AHV-Rente`))^2
+(cor(age_korrelation$age,age_korrelation$vote_result))^2
+cor.test(age_korrelation$age,age_korrelation$vote_result,paired = TRUE)
 
-cor.test(age_korrelation$`Anteil ü65-Jährige`,age_korrelation$`Ja-Anteil 13. AHV-Rente`,paired = TRUE)
-plot(age_korrelation$`Anteil ü65-Jährige`,age_korrelation$`Ja-Anteil 13. AHV-Rente`)
+
+
+ggscatter(age_korrelation, x = "age", y = "vote_result", 
+          add = "reg.line", conf.int = TRUE, 
+          cor.coef = TRUE, cor.method = "pearson",
+          xlab = "Anteil ü65-Jährige", ylab = "Ja-Anteil 13. AHV-Rente")
+
+
